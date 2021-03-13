a80b9b4b462118029e29db3ceceb089210121902
haibzhou <haibzhou@ebay.com>
2021-03-13 15:32:52 -0800

    update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ln -s $(pwd)/.vimrc ${HOME}/.vimrc
-ln -s $(pwd)/.vim ${HOME}/.vim
-ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
-ln -s $(pwd)/.tmux ${HOME}/.tmux
+rm -f ${HOME}/.vimrc && ln -s $(pwd)/.vimrc ${HOME}/.vimrc
+rm -f ${HOME}/.vim && ln -s $(pwd)/.vim ${HOME}/.vim
+rm -f ${HOME}/.tmux.conf && ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
+rm -f ${HOME}/.tmux && ln -s $(pwd)/.tmux ${HOME}/.tmux
+rm -f ${HOME}/.dircolors.dark && ln -s $(pwd)/dircolors.256dark ${HOME}/.dircolors.dark
