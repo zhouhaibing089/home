ccab82d9601f780a5050b624af8a6d89c47d75ee
zhouhaibing089 <zhouhaibing089@gmail.com>
2024-09-02 18:21:15 -0700

    slightly restructure

--- config.sh
+++ config.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 # vim.rc file
 rm -f ${HOME}/.vimrc && ln -s $(pwd)/.vimrc ${HOME}/.vimrc
 # vim color theme
 mkdir -p ${HOME}/.vim/colors
 rm -f ${HOME}/.vim/colors/solarized.vim
 ln -s $(pwd)/.vim/colors/solarized.vim ${HOME}/.vim/colors/solarized.vim
+
 # tmux
 rm -f ${HOME}/.tmux.conf && ln -s $(pwd)/.tmux.conf ${HOME}/.tmux.conf
+
 # tmux colors
 rm -drf ${HOME}/.tmux/colors
 mkdir -p ${HOME}/.tmux
@@ -15,6 +21,7 @@ ln -s $(pwd)/.tmux/colors ${HOME}/.tmux/colors
 # shell colors
 rm -f ${HOME}/.dircolors.dark && ln -s $(pwd)/dircolors.256dark ${HOME}/.dircolors.dark
 rm -f ${HOME}/.dircolors.light && ln -s $(pwd)/.dircolors.light ${HOME}/.dircolors.light
+
 # zshrc file
 rm -f ${HOME}/.zshrc && ln -s $(pwd)/.zshrc ${HOME}/.zshrc
 if [[ $(uname) == "Linux" ]]; then
