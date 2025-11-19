e026995648fcb185f30828797d869085ca570ece
Haibing Zhou <zhouhaibing089@gmail.com>
2025-11-18 22:28:07 -0800

    neovim: add plugins

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
+vim.opt.guifont = "JetBrainsMono Nerd Font:h16"
 vim.cmd("syntax on")
-vim.opt.termguicolors = false
 vim.opt.number = true
 vim.opt.colorcolumn = "80"
 vim.opt.textwidth = 80
@@ -8,34 +8,29 @@ vim.opt.splitbelow = true
 
 require("config.lazy")
 
--- telescope and its extensions
-require("telescope").setup {
-  extensions = {
-    file_browser = {
-      theme = "dropdown",
-      hijack_netrw = true,
-      mappings = {
-        ["i"] = {
-        },
-        ["n"] = {
-        },
-      },
-      grouped = true,
-      hidden = true,
-      hide_parent_dir = true,
-    },
-  },
-}
-require("telescope").load_extension "file_browser"
-
+-- telescope
 vim.keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>")
 vim.keymap.set('n', "<leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set('n', "<leader>fg", ":Telescope live_grep<CR>")
+vim.keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
 
 -- toggleterm
 require("toggleterm").setup {
-  direction = "float",
+  direction = "horizontal",
 }
 
 vim.keymap.set("n", "<leader>t", ":ToggleTerm<CR>")
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { noremap = true })
+
+-- bufferline
+require("bufferline").setup({
+  options = {
+    offsets = {
+      {
+        filetype = "neo-tree",
+        text = "Explorer",
+        text_align = "center",  -- or "left" / "right"
+      }
+    }
+  }
+})
