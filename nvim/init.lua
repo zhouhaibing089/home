0461dfefe6f83695c8285fd7850fe014d16f8556
Haibing Zhou <zhouhaibing089@gmail.com>
2025-11-22 22:57:15 -0800

    nvim: add signature plugin
    Add signature plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,3 +46,10 @@ vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
 vim.keymap.set("n", "<leader>f", function()
 	vim.lsp.buf.format({ async = true })
 end, opts)
+-- set capabilities for all installed lsp servers
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+for _, server in ipairs(require("mason-lspconfig").get_installed_servers()) do
+	vim.lsp.config(server, {
+		capabilities = capabilities,
+	})
+end
