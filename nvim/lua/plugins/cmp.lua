ada39089b7f3e3201d417cbc1e909ab564046580
Haibing Zhou <zhouhaibing089@gmail.com>
2025-11-22 21:51:49 -0800

    nvim: add nvim-cmp plugin
    Add nvim-cmp plugin.

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+		},
+		config = function()
+			local cmp = require("cmp")
+
+			cmp.setup({
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+					["<C-e>"] = cmp.mapping.abort(),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "buffer" },
+				},
+			})
+		end,
+	},
+}
