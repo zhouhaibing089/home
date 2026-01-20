599504c2a062892ccbc421e5feeca127e764446d
Haibing Zhou <zhouhaibing089@gmail.com>
2026-01-19 16:22:47 -0800

    undo transparent floating

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,11 +15,6 @@ vim.g.neovide_cursor_animate_in_insert_mode = false
 vim.g.neovide_cursor_animate_command_line = false
 vim.g.neovide_scroll_animation_far_lines = 0
 vim.g.neovide_scroll_animation_length = 0.00
--- make floating window semi-transparent
-vim.o.winblend = 20
-vim.o.pumblend = 20
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
 -- window navigation
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Window left" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Window down" })
--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -2,9 +2,6 @@ return {
 	"akinsho/toggleterm.nvim",
 	opts = {
 		direction = "float",
-		float_opts = {
-			winblend = 20,
-		},
 		shade_terminals = false,
 	},
 	version = "*",
