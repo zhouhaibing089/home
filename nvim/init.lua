f8731abd8011b3e16949149eca831639c8c490a3
Haibing Zhou <zhouhaibing089@gmail.com>
2026-01-19 16:21:03 -0800

    nvim: fg from visual selection

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,72 +39,94 @@ vim.api.nvim_create_autocmd("User", {
 })
 
 -- fzf
+local function get_visual_selection()
+	-- visual mode with selection that in the same line
+	local start_pos = vim.fn.getpos("'<")
+	local end_pos = vim.fn.getpos("'>")
+	local region = vim.fn.getregion(start_pos, end_pos, {
+		type = vim.fn.visualmode(),
+	})
+	return region[1]
+end
+
 local function find_files(opts, dir)
-	local query = ""
+	local query = nil
 	if opts.range == 2 and opts.line1 == opts.line2 then
-		-- visual mode with selection that in the same line
-		local start_pos = vim.fn.getpos("'<")
-		local end_pos = vim.fn.getpos("'>")
-		local region = vim.fn.getregion(start_pos, end_pos, {
-			type = vim.fn.visualmode(),
-		})
-		query = "'" .. region[1]
+		query = "'" .. get_visual_selection()
 	elseif #opts.fargs > 0 then
 		query = opts.args
+	else
+		query = ""
 	end
 	local preview = vim.fn["fzf#vim#with_preview"]({
 		options = { "--query", query },
 	})
 	vim.fn["fzf#vim#files"](dir, preview)
 end
+
 vim.api.nvim_create_user_command("Ff", function(opts)
 	find_files(opts, vim.fn.expand("."))
 end, { desc = "find files", nargs = "*", range = true })
 vim.api.nvim_create_user_command("FF", function(opts)
 	find_files(opts, vim.fn.expand("%:p:.:h"))
 end, { desc = "find files", nargs = "*", range = true })
--- RgHere searches current buffer directory
-vim.cmd([[
-command! -bang -nargs=* RgHere
-  \ call fzf#vim#grep(
-  \ "rg --column -n -L --no-heading --color=always -S -. -g '!.git/*' -- "
-  \   . fzf#shellescape(<q-args>)
-  \   . " "
-  \   . expand("%:p:.:h"),
-  \ fzf#vim#with_preview({ "options": [
-  \   "--delimiter", ":",
-  \   "--nth", "4..",
-  \   "--with-nth", "1,2,3,4..",
-  \ ] }),
-  \ <bang>0)
-]])
--- RgAll searches current work directory
-vim.cmd([[
-command! -bang -nargs=* RgAll
-  \ call fzf#vim#grep(
-  \ "rg --column -n -L --no-heading --color=always -S -. -g '!.git/*' -- "
-  \   . fzf#shellescape(<q-args>),
-  \ fzf#vim#with_preview({ "options": [
-  \   "--delimiter", ":",
-  \   "--nth", "4..",
-  \   "--with-nth", "1,2,3,4..",
-  \ ] }),
-  \ <bang>0)
-]])
--- Rg takes args as glob filter
-vim.cmd([[
-command! -bang -nargs=* Rg
-  \ call fzf#vim#grep(
-  \ "rg --column -n -L --no-heading --color=always -S -. -g '!.git/*' -g "
-  \   . fzf#shellescape(<q-args>)
-  \   . " -- ''",
-  \ fzf#vim#with_preview({ "options": [
-  \   "--delimiter", ":",
-  \   "--nth", "4..",
-  \   "--with-nth", "1,2,3,4..",
-  \ ] }),
-  \ <bang>0)
-]])
+
+local function grep_files(opts, dir)
+	local query = ""
+	local copts = "" -- additional command options
+	if opts.range == 2 and opts.line1 == opts.line2 then
+		-- query from visual selection
+		query = get_visual_selection()
+		copts = copts .. " -F "
+	end
+	local i, j = opts.args:find(" --", 1, true)
+	local g = ""
+	if i then
+		-- anything before `--` is glob filter
+		for _, v in ipairs(opts.fargs) do
+			if v == "--" then
+				break
+			end
+			g = " -g " .. vim.fn.shellescape(v)
+		end
+		-- anything after `--` is text to grep
+		rargs = opts.args:sub(j + 1):gsub("^%s+", "")
+		if rargs ~= "" then
+			query = rargs
+		end
+	elseif #opts.fargs > 0 then
+		query = opts.args
+	end
+	local preview = vim.fn["fzf#vim#with_preview"]({
+		options = {
+			"--query",
+			query,
+			"--delimiter",
+			":",
+			"--nth",
+			"4..",
+			"--with-nth",
+			"1,2,3,4..",
+		},
+	})
+	rg = "rg --column -n -L --no-heading --color=always -S -. "
+		.. "-g '!.git/*' "
+		.. g
+		.. copts
+		.. " -- "
+		.. vim.fn["fzf#shellescape"](query)
+		.. " "
+		.. dir
+	vim.fn["fzf#vim#grep"](rg, preview)
+end
+
+vim.api.nvim_create_user_command("Fg", function(opts)
+	grep_files(opts, vim.fn.expand("."))
+end, { desc = "find files", nargs = "*", range = true })
+vim.api.nvim_create_user_command("FG", function(opts)
+	grep_files(opts, vim.fn.expand("%:p:.:h"))
+end, { desc = "find files", nargs = "*", range = true })
+
 vim.env.BAT_THEME = "Solarized (dark)"
 vim.env.FZF_DEFAULT_COMMAND = "fd --type f --hidden --follow --exclude .git"
 vim.env.FZF_DEFAULT_OPTS = "--history=" .. os.getenv("HOME") .. "/.fzf_history"
@@ -117,8 +139,8 @@ vim.keymap.set({ "n", "x" }, "<leader>ff", ":Ff<CR>")
 vim.keymap.set({ "n", "x" }, "<leader>fF", ":FF<CR>")
 vim.keymap.set("n", "<leader>fb", ":Buffers<CR>")
 -- fG (current buffer's directory), fg (workspace)
-vim.keymap.set("n", "<leader>fG", ":RgHere<CR>")
-vim.keymap.set("n", "<leader>fg", ":RgAll<CR>")
+vim.keymap.set({ "n", "x" }, "<leader>fg", ":Fg<CR>")
+vim.keymap.set({ "n", "x" }, "<leader>fG", ":FG<CR>")
 vim.keymap.set("n", "<leader>ft", ":Tags<CR>")
 
 -- toggleterm
