f9c11b7c14d75681fcebda24bd4d02594270ca2e
Haibing Zhou <zhouhaibing089@gmail.com>
2026-01-24 17:01:25 -0800

    lualine: simplify sections

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,12 +2,17 @@ return {
 	"nvim-lualine/lualine.nvim",
 	opts = {
 		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
 			lualine_c = {
 				{
 					"filename",
 					path = 1,
 				},
 			},
+			lualine_x = { "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = {},
 		},
 		inactive_sections = {
 			lualine_c = {
@@ -16,6 +21,10 @@ return {
 					path = 1,
 				},
 			},
+		},
+		extensions = {
+			"neo-tree",
+			"toggleterm",
 		},
 	},
 	dependencies = { "nvim-tree/nvim-web-devicons" },
