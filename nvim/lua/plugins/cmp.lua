a5a85eb607bbcb7e932a0dc33d4b3861e839d968
Haibing Zhou <zhouhaibing089@gmail.com>
2026-01-27 06:51:14 -0800

    cmp: do not auto select

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -19,7 +19,7 @@ return {
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
 				}),
 			})
 		end,
