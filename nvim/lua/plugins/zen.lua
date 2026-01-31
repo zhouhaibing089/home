cad43f83d60a63cb2cbc83e6bc827a3fcd9248c0
Haibing Zhou <zhouhaibing089@gmail.com>
2026-01-31 14:32:06 -0800

    nvim: zen-mode

---
+++ nvim/lua/plugins/zen.lua
@@ -0,0 +1,14 @@
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		window = {
+			width = 1.0, -- take full editor width
+			height = 1.0, -- take full editor height
+		},
+		plugins = {
+			options = {
+				laststatus = 3,
+			},
+		},
+	},
+}
