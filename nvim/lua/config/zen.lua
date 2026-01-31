317d7bddf2293cec6fa1e9965e6d7198f261e998
Haibing Zhou <zhouhaibing089@gmail.com>
2026-01-31 14:44:01 -0800

    zen: smaller width

--- nvim/lua/config/zen.lua
+++ nvim/lua/config/zen.lua
@@ -1,1 +1,16 @@
-vim.keymap.set({ "n" }, "<leader>z", ":ZenMode<CR>", { desc = "toggle zen mode" })
+local zen = require("zen-mode")
+
+vim.keymap.set({ "n" }, "<leader>z", function()
+	zen.toggle({
+		window = {
+			width = 120,
+		},
+	})
+end, { desc = "toggle zen mode" })
+vim.keymap.set({ "n" }, "<leader>Z", function()
+	zen.toggle({
+		window = {
+			width = 1,
+		},
+	})
+end, { desc = "toggle zen mode" })
--- nvim/lua/plugins/zen.lua
+++ nvim/lua/plugins/zen.lua
@@ -1,10 +1,6 @@
 return {
 	"folke/zen-mode.nvim",
 	opts = {
-		window = {
-			width = 1.0, -- take full editor width
-			height = 1.0, -- take full editor height
-		},
 		plugins = {
 			options = {
 				laststatus = 3,
