dddaa7fa8fc1582662d300eb76f73e5ff8cb5e3e
Haibing Zhou <zhouhaibing089@gmail.com>
2026-02-07 21:57:03 -0800

    nvim: nr vs nR for neotree reveal

--- nvim/lua/config/neotree.lua
+++ nvim/lua/config/neotree.lua
@@ -11,6 +11,19 @@ vim.keymap.set("n", "<leader>nt", function()
 	})
 end, { desc = "Toggle neo-tree" })
 vim.keymap.set("n", "<leader>nr", function()
+	local t = vim.g.t or {}
+	nc.execute({
+		action = "focus",
+		source = "filesystem",
+		position = "float",
+		reveal = true,
+		dir = t.cwd or vim.fn.getcwd(),
+	})
+end, { desc = "Reveal file under current working directory" })
+-- This makes it faster when working in mono repository - like adding files or
+-- delete files in current directory doesn't need a full travesal from project
+-- root.
+vim.keymap.set("n", "<leader>nR", function()
 	nc.execute({
 		action = "focus",
 		source = "filesystem",
@@ -18,4 +31,4 @@ vim.keymap.set("n", "<leader>nr", function()
 		reveal = true,
 		dir = vim.fn.expand("%:p:h"),
 	})
-end, { desc = "Reveal file under cursor" })
+end, { desc = "Reveal file under current directory" })
