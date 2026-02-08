fb7b7dc1f8198127bca21c9ae4cf289209454350
Haibing Zhou <zhouhaibing089@gmail.com>
2026-02-07 17:12:06 -0800

    nvim: use different impl for neotree reveal

--- nvim/lua/config/neotree.lua
+++ nvim/lua/config/neotree.lua
@@ -1,5 +1,14 @@
 vim.keymap.set("n", "<leader>nt", ":Neotree source=filesystem toggle<CR>")
-vim.keymap.set("n", "<leader>nr", ":Neotree source=filesystem reveal<CR>")
+vim.keymap.set("n", "<leader>nr", function()
+	local file = vim.fn.expand("%:p")
+	require("neo-tree.command").execute({
+		action = "focus", -- Focus the tree
+		source = "filesystem", -- Use the filesystem source
+		position = "float", -- Open in a floating window
+		reveal_file = file, -- The specific file to highlight
+		reveal_force_cwd = true, -- Change CWD if the file is outside the current root
+	})
+end, { desc = "Reveal file under cursor in Neo-tree" })
 vim.api.nvim_create_autocmd("User", {
 	pattern = "NeoTreeRootChanged",
 	callback = function(args)
