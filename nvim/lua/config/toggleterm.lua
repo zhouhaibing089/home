90eeefc674c0293d52bbb865d387aae36baf627a
Haibing Zhou <zhouhaibing089@gmail.com>
2026-02-11 20:54:59 -0800

    nvim: tF and tB to pinned directory

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -9,10 +9,32 @@ vim.keymap.set("n", "<leader>tf", function()
 			direction = "float",
 		})
 	end
-	terms[count].dir = vim.t.cwd or vim.fn.getcwd()
+	terms[count].dir = vim.fn.getcwd()
 	terms[count]:toggle()
 end)
 vim.keymap.set("n", "<leader>tb", function()
+	local count = vim.fn.tabpagenr() * 10 + 2
+	if not terms[count] then
+		terms[count] = Terminal:new({
+			count = count,
+			direction = "horizontal",
+		})
+	end
+	terms[count].dir = vim.fn.getcwd()
+	terms[count]:toggle()
+end)
+vim.keymap.set("n", "<leader>tF", function()
+	local count = vim.fn.tabpagenr() * 10 + 1
+	if not terms[count] then
+		terms[count] = Terminal:new({
+			count = count,
+			direction = "float",
+		})
+	end
+	terms[count].dir = vim.t.cwd or vim.fn.getcwd()
+	terms[count]:toggle()
+end)
+vim.keymap.set("n", "<leader>tB", function()
 	local count = vim.fn.tabpagenr() * 10 + 2
 	if not terms[count] then
 		terms[count] = Terminal:new({
