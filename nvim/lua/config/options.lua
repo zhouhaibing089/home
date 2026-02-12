a0b9f27559a0755b168286cb63d40d186e5b1778
Haibing Zhou <zhouhaibing089@gmail.com>
2026-02-11 21:28:52 -0800

    nvim: remove textwidth

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,7 +3,6 @@ vim.opt.guifont = "JetBrainsMono Nerd Font:h14"
 vim.opt.number = true
 vim.opt.colorcolumn = "80"
 vim.opt.cursorline = true
-vim.opt.textwidth = 80
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.mmp = 5000
