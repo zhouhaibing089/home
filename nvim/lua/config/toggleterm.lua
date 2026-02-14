a5928258c5421dc7a3a04d720dae00b9f3d0a916
Haibing Zhou <zhouhaibing089@gmail.com>
2026-02-14 12:28:37 -0800

    nvim: C-t to close terminal

--- nvim/lua/config/toggleterm.lua
+++ nvim/lua/config/toggleterm.lua
@@ -1,47 +1,38 @@
 local Terminal = require("toggleterm.terminal").Terminal
 local terms = {}
 
-vim.keymap.set("n", "<leader>tf", function()
-	local count = vim.fn.tabpagenr() * 10 + 1
+function toggle(offset, direction, dir)
+	local count = vim.fn.tabpagenr() * 10 + offset
 	if not terms[count] then
 		terms[count] = Terminal:new({
 			count = count,
-			direction = "float",
+			direction = direction,
 		})
 	end
-	terms[count].dir = vim.t.cwd or vim.fn.getcwd()
+	terms[count].dir = dir
 	terms[count]:toggle()
+end
+
+vim.keymap.set("n", "<leader>tf", function()
+	toggle(1, "float", vim.t.cwd or vim.fn.getcwd())
 end, { desc = "toggle floating terminal" })
 vim.keymap.set("n", "<leader>tb", function()
-	local count = vim.fn.tabpagenr() * 10 + 2
-	if not terms[count] then
-		terms[count] = Terminal:new({
-			count = count,
-			direction = "horizontal",
-		})
-	end
-	terms[count].dir = vim.t.cwd or vim.fn.getcwd()
-	terms[count]:toggle()
+	toggle(2, "horizontal", vim.t.cwd or vim.fn.getcwd())
 end, { desc = "toggle bottom terminal" })
 vim.keymap.set("n", "<leader>tF", function()
-	local count = vim.fn.tabpagenr() * 10 + 1
-	if not terms[count] then
-		terms[count] = Terminal:new({
-			count = count,
-			direction = "float",
-		})
-	end
-	terms[count].dir = vim.fn.expand("%:p:h")
-	terms[count]:toggle()
+	toggle(1, "float", vim.fn.expand("%:p:h"))
 end, { desc = "toggle floating terminal from current buffer" })
 vim.keymap.set("n", "<leader>tB", function()
-	local count = vim.fn.tabpagenr() * 10 + 2
-	if not terms[count] then
-		terms[count] = Terminal:new({
-			count = count,
-			direction = "horizontal",
-		})
-	end
-	terms[count].dir = vim.fn.expand("%:p:h")
-	terms[count]:toggle()
+	toggle(2, "horizontal", vim.fn.expand("%:p:h"))
 end, { desc = "toggle bottom terminal from current buffer" })
+vim.keymap.set("t", "<C-t>", function()
+	local tt = require("toggleterm.terminal")
+	local id = vim.b.toggle_number or tt.get_focused_id()
+	if not id then
+		return
+	end
+	local term = tt.get(id, true)
+	if term then
+		term:close()
+	end
+end, { desc = "close current terminal" })
