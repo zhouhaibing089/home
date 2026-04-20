d6e546f89f6f7c3311e18ce0d8e68014400bd381
Haibing Zhou <haibzhou@openai.com>
2026-04-20 09:16:33 -0700

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "master",
-	lazy = false,
-	build = ":TSUpdate",
-	opts = {},
+    'neovim-treesitter/nvim-treesitter',
+    dependencies = { 'neovim-treesitter/treesitter-parser-registry' },
+    lazy = false,
+    build = ':TSUpdate',
 }
