179c30a29563507e3b40b55cce4aae1731ee3172
Haibing Zhou <haibzhou@openai.com>
2026-04-20 09:20:52 -0700

    Update treesitter.lua

--- nvim/lua/config/treesitter.lua
+++ nvim/lua/config/treesitter.lua
@@ -1,6 +1,10 @@
-require("nvim-treesitter.configs").setup({
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+require('neovim-treesitter').install { 'terraform', 'c', 'python', 'go' }
+vim.api.nvim_create_autocmd('FileType', {
+    pattern = { 'terraform', 'c', 'python', 'go' },
+    callback = function()
+        vim.treesitter.start()
+        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+        vim.wo.foldmethod = 'expr'
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
 })
