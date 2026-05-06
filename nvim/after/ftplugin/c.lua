73ddd9d94b8ca23c15e12c102f06a4c59571981d
Haibing Zhou <zhouhaibing089@gmail.com>
2026-05-05 22:45:55 -0700

    remove c lsp overrides

--- nvim/after/ftplugin/c.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set({ "n" }, "<leader>d", "<leader>fd", { buffer = true, remap = true, desc = "find definitions" })
-vim.keymap.set({ "v" }, "<leader>d", "<leader>fd", { buffer = true, remap = true, desc = "find definitions" })
-vim.keymap.set({ "n" }, "<leader>r", "<leader>fr", { buffer = true, remap = true, desc = "find references" })
-vim.keymap.set({ "v" }, "<leader>r", "<leader>fr", { buffer = true, remap = true, desc = "find references" })
